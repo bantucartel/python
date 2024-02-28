fb7626b6dc5e248f8b9bf36068e237672f1d9d44
bantucartel <160231682+bantucartel@users.noreply.github.com>
2024-02-28 13:53:58 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+message = "Hello " + name + ", you are " + age + " years old and you live in " + location + "."
+print(message)
